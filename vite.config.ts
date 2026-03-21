83ffa92a2d33bb33526986637e47950fecde3b91
Theo Dinh <thanh.dinh214@gmail.com>
2026-03-21 00:59:29 -0400

    Build and deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/Theo-Dinh-Portfolio/',
+  base: '/TDportfolio/',
   server: {
     host: "::",
     port: 8080,
